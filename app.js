0cc250471ac450a19c47fe2e725674b30a8d8069
Krish Shah <krish.life21@gmail.com>
2022-01-17 03:24:20 -0700

    another deploy fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const inventoryRoutes = require("./routes/inventoryRoutes");
 app.use(express.json());
 app.use(inventoryRoutes);
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 const server = http.createServer(app);
 const port = process.env.PORT || 4001;
 
